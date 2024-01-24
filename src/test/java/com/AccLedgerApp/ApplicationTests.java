e47c1e895a359273ea7772c3d66a740c4aaf5db0
clancycooper <dudeclancy@gmail.com>
2024-01-23 21:58:55 -0500

    First commit

---
+++ src/test/java/com/AccLedgerApp/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AccLedgerApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
